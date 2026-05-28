c8dd944f33b4a8e240f6aeb617cc4e4927b4ead8
Nicolas Bcg <bocage.nicolas11@gmail.com>
2026-05-28 11:01:26 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/webhook': {
+        target: 'https://n8n.srv761186.hstgr.cloud',
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
 })
